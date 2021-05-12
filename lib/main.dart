50fe118b4fee476b466e50ccf19b2eb2d97de95a
AxlamJ <75744162+AxlamJ@users.noreply.github.com>
2021-05-12 23:52:06 +0500

    geo

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 
       ),
       body: Center(
-        child: Text("Muhammad Aslam Jalbani"),
+        child: Text("Muhammad Aslam Geo"),
         ),
       ),
 
