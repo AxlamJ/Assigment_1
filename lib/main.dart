9af6d5149acf4286a3fa9e2413e52cb09d9af031
AxlamJ <aslam.engineer3@gmail.com>
2021-05-13 00:01:25 +0500

    azay

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 
       ),
       body: Center(
-        child: Text("Muhammad Aslam Qayoom"),
+        child: Text("Muhammad Aslam Ayaz"),
         ),
       ),
 
